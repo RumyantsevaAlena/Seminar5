2e203e802dee376c05df4c496f1f1a11077496c7
RumyantsevaAlena <kaluga1993@mail.ru>
2023-02-01 21:49:14 +0300

    Решение домашки к 5 семинару

---
+++ task01/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+
+//[345, 897, 568, 234] -> 2
+
+int[] FullArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int [size];
+    Random rand = new Random();
+    for (int i=0; i< arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("["+ string.Join (", ", arr)+"]");
+}
+
+int[] array = FullArrayWithRandomNumbers(6, 100, 1000);
+System.Console.WriteLine("Изначальный массив: ");
+PrintArray(array);
+int count = 0;
+
+for(int i=0; i<array.Length;i++ )
+{
+   if (array[i]%2==0)
+   count++;
+}
+System.Console.WriteLine($"{count}");
---
+++ task02/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] FullArrayWithRandomNumbers(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int [size];
+    Random rand = new Random();
+    for (int i=0; i< arr.Length; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange+1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    System.Console.WriteLine("["+ string.Join (", ", arr)+"]");
+}
+
+int[] arr = FullArrayWithRandomNumbers(6, -50, 50);
+System.Console.WriteLine("Наш массив: ");
+PrintArray(arr);
+int sum = 0;
+
+for (int z = 0; z < arr.Length; z+=2)
+    sum = sum + arr[z];
+
+    Console.WriteLine($"всего {arr.Length} чисел, сумма элементов на нечётных позициях = {sum}");
